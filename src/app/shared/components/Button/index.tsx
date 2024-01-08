28d694873dfe44e08fb6265bc93629183820b661
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-01-08 11:50:16 +1100

    improved button style

--- src/app/shared/components/Button/index.tsx
+++ src/app/shared/components/Button/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import clsx from 'clsx'
+
 interface ButtonProps {
   children: React.ReactNode,
   className?: string,
@@ -10,7 +12,16 @@ const Button = (props: ButtonProps) => {
   const { className, onClick } = props
 
   return (
-    <button className={className} onClick={onClick}>
+    <button
+      className={clsx(
+        "border border-slate-900 rounded-lg",
+        "p-2 mt-4",
+        "bg-green-600 text-white",
+        "hover:bg-green-700",
+        className,
+      )}
+      onClick={onClick}
+    >
       {props.children}
     </button>
   )
